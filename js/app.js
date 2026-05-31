65ed3f135bdc738e8cd799f27bf5a20504b35e36
eongmang <eongmang@gmail.com>
2026-05-31 10:56:00 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+function toggleMenu() {
 
+    document
+        .getElementById("sideMenu")
+        .classList.toggle("active");
+
+    document
+        .getElementById("overlay")
+        .classList.toggle("active");
+}
+
+fetch("menu.html")
+.then(response => response.text())
+.then(data => {
+    document.getElementById("menu-container").innerHTML = data;
+});
